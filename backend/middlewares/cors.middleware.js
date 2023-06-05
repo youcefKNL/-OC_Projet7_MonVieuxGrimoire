9e4b4a61d23867e6ed4e7afb70112db58100ad4e
youcefKNL <khenichil_youcef@hotmail.fr>
2023-06-06 00:01:12 +0200

    deploy Render

--- backend/middlewares/cors.middleware.js
+++ backend/middlewares/cors.middleware.js
@@ -1,17 +1,14 @@
 const cors = require("cors"); // Package qui gère le CORS des en-tête
-
 // Configurez les options CORS selon vos besoins
 const corsOptions = {
   // Autoriser toutes les origines
-  origin: ["http://localhost:3000", "https://youcefknl.github.io/"],
+  origin: ["http://localhost:3000", "https://youcefknl.github.io"],
   // En-têtes autorisés
   allowedHeaders:
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
   // Méthodes HTTP autorisées
   methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
 };
-
 // Créez le middleware CORS avec les options configurées
 const corsMiddleware = cors(corsOptions);
-
 module.exports = corsMiddleware;
